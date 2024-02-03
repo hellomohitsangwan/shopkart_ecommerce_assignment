6dc49ce586ab0c8a9cc55dc3c82739ac8ea1dd57
hellomohitsangwan <hellomohitsangwan@gmail.com>
2024-02-04 00:17:41 +0530

    card bug fix

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,8 +1,6 @@
 import {
     CART_ADD_ITEM,
     CART_DELETE_ITEM,
-    CART_SAVE_PAYMENT_METHOD,
-    CART_SAVE_SHIPPING_ADDRESS,
   } from "../constants/cartConstants";
   
   export const cartReducer = (
--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart, removeFromCart } from "../actions/cartActions";
-import cartBackground from "../assets/cartBackground.svg";
 import "./Screen.css";
 import {
   Row,
@@ -16,6 +15,7 @@ import {
 import Message from "../components/Message";
 import Footer from "../components/Footer";
 import ComingSoonScreen from "./ComingSoonScreen";
+import Modal from "react-bootstrap/Modal";
 
 const CartScreen = ({ match, location, history }) => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -43,7 +43,8 @@ const CartScreen = ({ match, location, history }) => {
     dispatch(removeFromCart(id));
   };
   const checkoutHandler = () => {
-    history.push("/new");
+    // history.push("/new");
+    setShow(true);
   };
   useEffect(() => {
     if (productId) {
@@ -53,13 +54,19 @@ const CartScreen = ({ match, location, history }) => {
     console.log(qty);
   }, [dispatch, productId, qty, history]);
   console.log(cartItems);
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       {windowWidth >= 1000 ? (
         <div>
           <Row>
             <Col md={8}>
-              <h1 className="cart-title">Your Shopping cart</h1>
+              <h1 className="cart-title">My Cart</h1>
               <div className="green-underline"></div>
               <div className="black-underline"></div>
               {cartItems.length === 0 ? (
@@ -134,9 +141,18 @@ const CartScreen = ({ match, location, history }) => {
                           </Col>
                         </div>
                       </Row>
+
                       <div className="black-underline"></div>
                     </ListGroup.Item>
                   ))}
+                  <Button
+                    type="button"
+                    className="btn btn-block checkout-btn"
+                    disabled={cartItems.length === 0}
+                    onClick={checkoutHandler}
+                  >
+                    Place Order
+                  </Button>
                 </ListGroup>
               )}
             </Col>
@@ -163,25 +179,26 @@ const CartScreen = ({ match, location, history }) => {
                   </ListGroup.Item>
                   <div className="gap"></div>
                   <div className="checkout-underline"></div>
-
-                  <ListGroup.Item
-                    style={{ zIndex: "1000" }}
-                    type="button"
-                    onClick={checkoutHandler}
-                  >
-                    <Button
-                      type="button"
-                      className="btn btn-block checkout-btn"
-                      disabled={cartItems.length === 0}
-                      onClick={checkoutHandler}
-                    >
-                      Proceed To Checkout
-                    </Button>
-                  </ListGroup.Item>
                 </ListGroup>
               </Card>
             </Col>
           </Row>
+          <Modal className="cm" show={show} onHide={handleClose}>
+            <Modal.Header>
+              <Modal.Title>Order Placed Successfully</Modal.Title>
+            </Modal.Header>
+            <div className="mi">
+              {" "}
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z" />
+              </svg>
+            </div>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
       ) : (
         <ComingSoonScreen />
