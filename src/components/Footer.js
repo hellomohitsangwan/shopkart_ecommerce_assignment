5c1118c367a7f13e227abf74e6817dabf2129aec
hellomohitsangwan <hellomohitsangwan@gmail.com>
2024-02-03 22:38:55 +0530

    product card ui update and fix and refactor

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./component.css";
+const Footer = () => {
+  return (
+    <footer className="main-footer">
+      <div className="links container">
+        <p>Procommerce &copy;, 2023 All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+//redux files
+import { listProducts } from "../actions/productAction";
+import { useDispatch, useSelector } from "react-redux";
+import Slider from "../components/Slider";
+import downArrow from "../assets/downArrow.svg";
+import Footer from "../components/Footer";
 
-function HomeScreen() {
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
+
+const HomeScreen = ({ match }) => {
+  const myRef = useRef(null);
+  const executeScroll = () => scrollToRef(myRef);
+  const keyword = match.params.keyword;
+
+  const productList = useSelector((state) => state.productList); //choose from whatever in our store
+  const { loading, products, error } = productList;
+  console.log(products);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
   return (
-    <div>HomeScreen</div>
-  )
-}
+    <>
+    <div className="container">
+      {/* <Slider /> */}
+      {/* <div className="allProducts-title">
+        <p>All Products</p>
+        <button className="btn" onClick={executeScroll}>
+          <img src={downArrow} alt="" className="arrow" />
+        </button>
+        <div className="ref" ref={myRef}></div>
+        <div className="gap1"></div>
+      </div> */}
+      {products?.length !== 0 ? (
+        <div className="latest">
+          {/* <p className="latest-product">Latest Products</p> */}
+          <div className="underline1"></div>
+        </div>
+      ) : (
+        !loading && (
+          <>
+            <h1 className="text-center , py-3">Sorry no results found !!</h1>
+            <p className="text-center">
+              please check your spelling or try searching for something else
+            </p>
+          </>
+        )
+      )}
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger" children={error} />
+      ) : (
+        <Row className="mt-4">
+          {products?.map((product) => (
+            <Col key={product?._id} xs={9} sm={9} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+      
+    </div>
+    <Footer /></>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
