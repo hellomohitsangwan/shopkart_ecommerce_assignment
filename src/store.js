faa9d2a699ffd76d4e02dcf78c544e9bf3348929
hellomohitsangwan <hellomohitsangwan@gmail.com>
2024-02-03 17:16:51 +0530

    user reducer and actions

--- src/store.js
+++ src/store.js
@@ -1,17 +1,36 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import {
+    userLoginReducer,
+    userRegisterReducer,
+    userDetailReducer,
+  } from "./reducer/userReducer";
 
 
 const reducer = combineReducers({
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailReducer,
 });
 
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+    userLogin: {
+    userInfo: userInfoFromStorage,
+    },
+};
 
 const middleware = [thunk];
 
 const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(...middleware))
+    reducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
